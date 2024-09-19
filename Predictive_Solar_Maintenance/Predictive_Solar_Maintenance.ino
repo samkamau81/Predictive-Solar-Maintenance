d3ff6b0f669b1344d2484a6bac2f0a5156c8e1b4
Mugi  Mugendi <mugiendi318@gmail.com>
2024-09-19 10:43:31 +0300

    update

--- Predictive_Solar_Maintenance/Predictive_Solar_Maintenance.ino
+++ Predictive_Solar_Maintenance/Predictive_Solar_Maintenance.ino
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #include <Wire.h>
 #include <BH1750.h>
 #include <DHT11.h>
@@ -72,3 +73,79 @@ void loop() {
 
   delay(2000);  // Delay between readings
 }
+=======
+#include <Wire.h>
+#include <BH1750.h>
+#include <Adafruit_AHT10.h>
+#include "communication.h"
+
+#define LED_PIN 2
+
+Adafruit_AHT10 aht;
+BH1750 lightMeter;
+
+//int voltageSensorPin = 33;
+int currentSensorPin = 34;
+//int lightSensorPin = A2;
+
+// Wi-Fi credentials
+const char* ssid = "Mugi";          // Replace with your WiFi SSID
+const char* password = "AquaCoin1";  // Replace with your WiFi password
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin(22,21);
+  aht.begin();
+  lightMeter.begin();
+  pinMode(LED_PIN, OUTPUT);
+
+  // Initialize WiFi
+  setupWiFi(ssid, password);
+}
+
+void loop() {
+  // Read Voltage
+ // int voltageValue = analogRead(voltageSensorPin);
+ // float voltage = (voltageValue / 1023.0) * 5.0 * (21.5 / 5.0);  // Adjust based on voltage divider
+
+  // Read Current
+  int currentValue = analogRead(currentSensorPin);
+  float current = (currentValue / 1023.0) * 5.0 * 0.67;  // Adjust based on sensor specs
+
+  // Read Dust Density
+  digitalWrite(LED_PIN, LOW);  // Turn on the LED
+  delayMicroseconds(280);  // Wait for sensor stabilization
+  
+  delayMicroseconds(40);  // Delay to avoid spikes
+  digitalWrite(LED_PIN, HIGH);  // Turn off the LED
+  
+
+  // Read Temperature and Humidity
+  sensors_event_t humidity, temp;
+  aht.getEvent(&humidity, &temp);// populate temp and humidity objects with fresh data
+  Serial.print("Temperature: "); Serial.print(temp.temperature); Serial.println(" degrees C");
+  Serial.print("Humidity: "); Serial.print(humidity.relative_humidity); Serial.println("% rH");
+
+
+  // Read Light Intensity
+  float lightIntensity = lightMeter.readLightLevel();
+
+  // Print data to Serial
+  Serial.print("Voltage: ");
+  //Serial.print(voltage);
+  Serial.print(" V, Current: ");
+  Serial.print(current);
+   Serial.print(" %, Temperature: ");
+  Serial.print(temp.temperature);
+  Serial.print(" C, Humidity: ");
+  Serial.print(humidity.relative_humidity);
+  Serial.print(" %, Light Intensity: ");
+  Serial.print(lightIntensity);
+  Serial.println(" lx");
+
+  // Send data to server
+  //sendDataToServer(voltage, current, temperature, humidity, lightIntensity);
+
+  delay(2000);  // Delay between readings
+}
+>>>>>>> Stashed changes
